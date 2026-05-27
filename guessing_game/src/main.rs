9a9e6f7c52215f93ca2295c3eeb1cfa56366864b
h4ckl07d <lawalgbolahan042@gmail.com>
2026-05-27 07:27:10 +0100

    added comparison feature

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,20 +1,31 @@
-use std ::io;
+use std::cmp::Ordering;
+
+use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("welcome to guessing game");
 
-    println!("The secret number is: {secret_number}");
+    let secret_number= rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess!");
+    println!("The secret_number is: {secret_number}");
 
+    println!("Enter your guess: ");
     let mut guess = String::new();
 
     io::stdin()
-        .read_line( &mut guess)
+        .read_line(&mut guess)
         .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+
+    let guess:i32 = guess.trim().parse().expect("Please Type a number!");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win"),
+    }
 
 }
