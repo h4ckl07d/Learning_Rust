e8433d213d5b4897311552194b7f18fff9020ae4
h4ckl07d <lawalgbolahan042@gmail.com>
2026-05-28 14:47:35 +0100

    implemented error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,23 +9,32 @@ fn main() {
 
     let secret_number= rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret_number is: {secret_number}");
+    //println!("The secret_number is: {secret_number}");
 
-    println!("Enter your guess: ");
-    let mut guess = String::new();
+    loop{
+        println!("Enter your guess: ");
+        let mut guess = String::new();
 
-    io::stdin()
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess:i32 = guess.trim().parse().expect("Please Type a number!");
+         let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+         };
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win"),
+         match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            } 
+        }
+
     }
 
 }
